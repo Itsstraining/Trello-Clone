4fbd179f1d74d0e6b5162f528d091b0aa94dadbc
HuynhLeHuy <huy01647287720@gmail.com>
2020-08-07 10:59:08 +0700

    git

--- profile-page/const.js
+++ profile-page/const.js
@@ -1,7 +1,6 @@
 const primary = '#7c4dff'; // the vao header 
 const secondary = '3d5afe'; // button
 const text = '##eeeeee' // the vao text 
-
 function initFocusButton() {
     let buttonarr = document.getElementsByClassName("bnt-tag");
 
